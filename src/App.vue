83f8a9e7700d25e160d0ad0a308088360c64f486
carlo697 <carlos.07888@gmail.com>
2026-03-01 19:09:09 -0400

    feat: generate resistor grid for printing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <script setup lang="ts">
+import { computed, ref } from "vue";
+import ResistorCard, { type Tolerance } from "./components/ResistorCard.vue";
 
+const series: Record<Tolerance, number[]> = {
+  5: [
+    1.0, 1.1, 1.2, 1.3, 1.5, 1.6, 1.8, 2.0, 2.2, 2.4, 2.7, 3.0, 3.3, 3.6, 3.9,
+    4.3, 4.7, 5.1, 5.6, 6.2, 6.8, 7.5, 8.2, 9.1,
+  ],
+};
+
+const multipliers = [
+  0.1, 1, 10, 100, 1_000, 10_000, 100_000, 1_000_000, 10_000_000, 100_000_000,
+];
+
+const tolerance = ref<Tolerance>(5);
+const seriesToUse = computed(() => series[tolerance.value]);
 </script>
 
 <template>
-  <div>
-    <ResistorCard />
+  <div class="flex flex-wrap">
+    <template v-for="multiplier in multipliers">
+      <ResistorCard
+        v-for="value in seriesToUse"
+        :value="value * multiplier"
+        :tolerance="5"
+        style="
+          width: 18mm;
+          height: 10mm;
+          padding-left: 1.5mm;
+          padding-right: 1.5mm;
+          padding-bottom: 1.5mm;
+        "
+      />
+    </template>
   </div>
 </template>
--- src/components/ResistorCard.vue
+++ src/components/ResistorCard.vue
@@ -1,24 +1,183 @@
 <script setup lang="ts">
+import { computed } from "vue";
+import resistorImage from "../assets/resistor.png";
 
+export type Tolerance = 5;
+
+const props = defineProps<{
+  value: number;
+  tolerance: Tolerance;
+}>();
+
+const formattedValue = computed(() => {
+  let prefix = "";
+  let value = props.value;
+
+  if (props.value >= 1_000_000_000) {
+    value = props.value / 1_000_000_000;
+    prefix = "G";
+  } else if (props.value >= 1_000_000) {
+    value = props.value / 1_000_000;
+    prefix = "M";
+  } else if (props.value >= 1_000) {
+    value = props.value / 1_000;
+    prefix = "k";
+  }
+
+  return `${parseFloat(value.toFixed(4))} ${prefix}Ω`;
+});
+
+const black = "#000";
+const brown = "#964b00";
+const red = "#ff0000";
+const orange = "#ffa500";
+const yellow = "#ffff00";
+const green = "#9acd32";
+const blue = "#6495ed";
+const violet = "#9400d3";
+const grey = "#a0a0a0";
+const white = "#fff";
+
+const gold = "#cfb53b";
+const silver = "#c0c0c0";
+
+const numberToColor: Record<number, string> = {
+  0: black,
+  1: brown,
+  2: red,
+  3: orange,
+  4: yellow,
+  5: green,
+  6: blue,
+  7: violet,
+  8: grey,
+  9: white,
+};
+
+const toleranceToColor: Record<Tolerance, string> = {
+  5: gold,
+};
+
+const numericPart = computed(() => {
+  let number = props.value;
+  if (number < 1) {
+    number =
+      props.value * Math.pow(10, Math.ceil(Math.abs(Math.log10(number))));
+  }
+
+  return number.toString().replace(/[^0-9]/g, "");
+});
+
+const band1Color = computed(() => {
+  const number = numericPart.value[0];
+  return numberToColor[number ? parseInt(number) : 0];
+});
+
+const band2Color = computed(() => {
+  const number = numericPart.value[1];
+  return numberToColor[number ? parseInt(number) : 0];
+});
+
+const logarithmToColor: Record<number, string> = {
+  [-1]: silver,
+  [0]: gold,
+  [1]: black,
+  2: brown,
+  3: red,
+  4: orange,
+  5: yellow,
+  6: green,
+  7: blue,
+  8: violet,
+  9: grey,
+  10: white,
+};
+
+const multiplierColor = computed(() => {
+  const logarithm = Math.log10(props.value);
+  const rounded = Math.floor(logarithm);
+
+  return logarithmToColor[rounded];
+});
 </script>
 
 <template>
-  <div>
-    
+  <div class="border-[0.5px]">
+    <div class="resistor-card flex flex-col items-center">
+      <div class="resistor-card-value font-semibold">{{ formattedValue }}</div>
+
+      <div class="resistor-card-tolerance font-medium">
+        ±{{ tolerance }}% (J)
+      </div>
+
+      <div class="resistor-card-img aspect-300/61 relative">
+        <img :src="resistorImage" />
+
+        <div class="band band-1" :style="{ backgroundColor: band1Color }"></div>
+
+        <div class="band band-2" :style="{ backgroundColor: band2Color }"></div>
+
+        <div
+          class="band band-3"
+          :style="{ backgroundColor: multiplierColor }"
+        ></div>
+
+        <div
+          class="band band-4"
+          :style="{ backgroundColor: toleranceToColor[props.tolerance] }"
+        ></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.resistor-card {
+  width: 100%;
+  height: 100%;
+  container-type: size;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.resistor-card-value {
+  font-size: 40cqh;
+  line-height: 1;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.resistor-card-tolerance {
+  font-size: 20cqh;
+  line-height: 1;
+}
+
+.resistor-card-img {
+  height: 40cqh;
+}
+
+.band {
+  position: absolute;
+  width: 4%;
+}
+
+.band-1 {
+  height: 96%;
+  top: 2.5%;
+  left: 25%;
+}
+
+.band-2 {
+  height: 80%;
+  top: 10%;
+  left: 37%;
+}
+
+.band-3 {
+  height: 80%;
+  top: 10%;
+  left: 48%;
+}
+
+.band-4 {
+  height: 80%;
+  top: 10%;
+  left: 66%;
 }
 </style>
