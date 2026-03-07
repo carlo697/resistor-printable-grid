4166d0e7de9b8c582ff3836c3ead9282750b633e
carlo697 <carlos.07888@gmail.com>
2026-03-06 22:35:34 -0400

    feat: add basic modal component

---
+++ src/components/modals/Modal.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{ maxWidth?: number }>();
+
+const isOpen = defineModel<boolean>({ required: true });
+</script>
+
+<template>
+  <Teleport to="#modals">
+    <div
+      v-show="isOpen"
+      class="fixed top-0 left-0 w-full h-full bg-gray-800/50 flex items-center justify-center p-6"
+    >
+      <div
+        class="bg-white rounded-xl p-6 grid grid-cols-1 gap-4"
+        :style="{ maxWidth: `${maxWidth}px` }"
+      >
+        <slot />
+      </div>
+    </div>
+  </Teleport>
+</template>
