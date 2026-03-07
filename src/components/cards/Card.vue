f51d0541f826992f07e92df02cc88c234413d5bf
carlo697 <carlos.07888@gmail.com>
2026-03-06 22:36:11 -0400

    feat: add basic card component

---
+++ src/components/cards/Card.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { Component } from "vue";
+
+withDefaults(defineProps<{ is?: string | Component }>(), { is: "div" });
+</script>
+
+<template>
+  <component :is="is" class="border rounded-lg border-gray-200 p-2">
+    <slot />
+  </component>
+</template>
