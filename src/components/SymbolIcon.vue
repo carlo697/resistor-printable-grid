e126e844384b24f4f4c8bbbf1a7fc656a41e01a8
carlo697 <carlos.07888@gmail.com>
2026-03-10 21:07:29 -0400

    feat: add 9 new symbols

--- src/components/SymbolIcon.vue
+++ src/components/SymbolIcon.vue
@@ -3,6 +3,8 @@ import type { Component } from "vue";
 import capacitor from "../assets/capacitor.svg?component";
 import potentiometer from "../assets/potentiometer.svg?component";
 import quartz from "../assets/quartz.svg?component";
+import button from "../assets/button.svg?component";
+import limit_switch from "../assets/limit_switch.svg?component";
 import relay from "../assets/relay.svg?component";
 import led_diode from "../assets/led_diode.svg?component";
 import zener_diode from "../assets/zener_diode.svg?component";
@@ -13,15 +15,24 @@ import diac from "../assets/diac.svg?component";
 import triac from "../assets/triac.svg?component";
 import ldr from "../assets/ldr.svg?component";
 import op_amp from "../assets/op_amp.svg?component";
+import schmitt_trigger from "../assets/schmitt_trigger.svg?component";
+import and from "../assets/and.svg?component";
+import nand from "../assets/nand.svg?component";
+import or from "../assets/or.svg?component";
+import nor from "../assets/nor.svg?component";
 import dip_8 from "../assets/dip_8.svg?component";
+import dip_14 from "../assets/dip_14.svg?component";
 import dip_16 from "../assets/dip_16.svg?component";
 import dip_32 from "../assets/dip_32.svg?component";
+import to_92 from "../assets/to_92.svg?component";
 import to_220 from "../assets/to_220.svg?component";
 
 export const symbolTypes = [
   "capacitor",
   "potentiometer",
   "quartz",
+  "button",
+  "limit_switch",
   "relay",
   "led_diode",
   "zener_diode",
@@ -32,9 +43,16 @@ export const symbolTypes = [
   "triac",
   "ldr",
   "op_amp",
+  "schmitt_trigger",
+  "and",
+  "nand",
+  "or",
+  "nor",
   "dip_8",
+  "dip_14",
   "dip_16",
   "dip_32",
+  "to_92",
   "to_220",
 ] as const;
 export type SymbolType = (typeof symbolTypes)[number];
@@ -51,6 +69,12 @@ export const symbols: Record<
   },
   quartz: {
     svg: quartz,
+  },
+  button: {
+    svg: button,
+  },
+  limit_switch: {
+    svg: limit_switch,
   },
   relay: {
     svg: relay,
@@ -83,14 +107,35 @@ export const symbols: Record<
   op_amp: {
     svg: op_amp,
   },
+  schmitt_trigger: {
+    svg: schmitt_trigger,
+  },
+  and: {
+    svg: and,
+  },
+  nand: {
+    svg: nand,
+  },
+  or: {
+    svg: or,
+  },
+  nor: {
+    svg: nor,
+  },
   dip_8: {
     svg: dip_8,
+  },
+  dip_14: {
+    svg: dip_14,
   },
   dip_16: {
     svg: dip_16,
   },
   dip_32: {
     svg: dip_32,
+  },
+  to_92: {
+    svg: to_92,
   },
   to_220: {
     svg: to_220,
